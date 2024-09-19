ea3cb99ea4d2c6280f0b7f1dacfbd631edc66896
Najifa Montaha Artika <najifamontahaartika@gmail.com>
2024-09-20 00:19:08 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,29 @@
+document.getElementById('login_button').addEventListener('click', function(event){
+    event.preventDefault()
+    // console.log('login Button cliked')
+
+
+    const phone_Number = document.getElementById('number_input').value
+    const pin_number = document.getElementById('password_input').value
+
+    console.log(phone_Number, pin_number)
+
+    if(phone_Number === '10' && pin_number === '3478'){
+        console.log('your Phone number & pin Number is correct')
+        window.location.href = '/home.html'
+    }
+    else{
+        alert('please check your Number of password')
+    }
+
+
+
+})
+
+
+
+
+
+
+
+
